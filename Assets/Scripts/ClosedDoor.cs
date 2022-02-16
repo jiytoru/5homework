ba85da2bac87aa5224674e5d166c55044f55ff88
jiytoru <semblavr@gmail.com>
2022-02-16 19:44:05 +0600

    5 практическое задание

---
+++ Assets/Scripts/ClosedDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosedDoor : MonoBehaviour
+{
+    public bool isClosed = true;
+
+    
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.CompareTag("Key"))
+        {
+            isClosed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PickUpObjects.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObjects : MonoBehaviour
+{
+    public GameObject keys;
+    private bool hasKey = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hasKey = true;
+            PickUpObjects.Destroy(keys);
+        }
+    }
+
+    
+}
